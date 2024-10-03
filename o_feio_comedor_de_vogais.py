a83d2cc386a8ad87710e0a524f05ed5c08c8ea31
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-03 13:38:35 -0300

    Create o_feio_comedor_de_vogais.py

---
+++ o_feio_comedor_de_vogais.py
@@ -0,0 +1,11 @@
+user_word = input("Insira uma palavra: ")
+
+user_word = user_word.upper()
+
+for letter in user_word:
+    
+    if letter in ['A', 'E', 'I', 'O', 'U']:
+        
+        continue
+    
+    print(letter)
